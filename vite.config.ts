4b2faf5acb2e31b2f5f6f4fef359c4102c820fb1
Mukeshgn <mukeshudatha18@gmail.com>
2025-11-15 00:03:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // IMPORTANT: set base to your repo name so assets load correctly on GitHub Pages
+  base: "/mukesh_portfolio/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
